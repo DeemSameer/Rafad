921d0baf02fc791369dc90e2a47e0da594172544
Deem Sameer AlDekhial <66889295+DeemSameer@users.noreply.github.com>
2020-09-10 21:12:56 +0300

    Initial commit

---
+++ app/src/main/java/com/example/rafad/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.rafad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+Timer timer ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent (MainActivity.this, login.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000 );
+
+
+/*
+        Button button = (Button) findViewById(R.id.mainButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                open_SignUp();
+            }
+        });
+
+    }
+
+    public void open_SignUp(){
+        Intent intent = new Intent(this, signUpBase.class);
+        startActivity(intent);
+    }
+
+ */
+
+    }
+}
