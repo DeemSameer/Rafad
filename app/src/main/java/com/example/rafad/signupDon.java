366873c20849429a1fc4a731dc34cd3d49ddc3c3
a-alsultan <arob2604@gmail.com>
2020-09-11 19:35:25 +0300

    add signup donator

--- app/src/main/java/com/example/rafad/signupDon.java
+++ app/src/main/java/com/example/rafad/signupDon.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class sign_up_donator extends AppCompatActivity {
+public class signupDon extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up_donator);
+        setContentView(R.layout.activity_signup_don);
     }
 }
