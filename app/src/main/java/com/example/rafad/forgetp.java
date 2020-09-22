ea6eab5322a825634c9d967993f3ce09a4415895
Deem Sameer AlDekhial <66889295+DeemSameer@users.noreply.github.com>
2020-09-23 01:29:36 +0300

    Merge branch 'master' of https://github.com/DeemSameer/Rafad into master
     Conflicts:
    	app/src/main/java/com/example/rafad/MainActivity.java
    
    Some updates

--- app/src/main/java/com/example/rafad/forgetp.java
+++ app/src/main/java/com/example/rafad/forgetp.java
@@ -28,30 +28,40 @@ public class forgetp extends AppCompatActivity {
         m=FirebaseAuth.getInstance();
         fo=findViewById(R.id.button2);
         ed=findViewById(R.id.editTextTextPersonName2);
+        Button button4 = (Button) findViewById(R.id.but);
 
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(forgetp.this, login.class));
+                finish();
+            }
+        });
         fo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String userem=ed.getText().toString();
                 if(TextUtils.isEmpty(userem)){
-                    Toast.makeText(forgetp.this," الرجاء كتابة بريدك الإلكتروني ",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(forgetp.this," الرجاء كتابة بريدك الإلكتروني ",Toast.LENGTH_LONG).show();
                 }
                 else{
                     m.sendPasswordResetEmail(userem).addOnCompleteListener(new OnCompleteListener<Void>() {
                         @Override
                         public void onComplete(@NonNull Task<Void> task) {
                             if(task.isSuccessful()){
-                                Toast.makeText(forgetp.this," تم ارسال رسالة إعادة تعيين كلمة المرور الى بريدك الإلكتروني ",Toast.LENGTH_SHORT).show();
+                                Toast.makeText(forgetp.this," تم ارسال رسالة إعادة تعيين كلمة المرور الى بريدك الإلكتروني ",Toast.LENGTH_LONG).show();
                                 startActivity(new Intent(forgetp.this,login.class));
                                 finish();
 
                             }
                             else{
                                 if (task.getException().getMessage().equals("There is no user record corresponding to this identifier. The user may have been deleted."))
-                                    Toast.makeText(forgetp.this, " الايميل غير موجود لدينا يرجى تسجيل حساب جديد " , Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(forgetp.this, " الايميل غير موجود لدينا يرجى تسجيل حساب جديد " , Toast.LENGTH_LONG).show();
+                                else if (task.getException().getMessage().equals("The email address is badly formatted."))
+                                    Toast.makeText(forgetp.this, " يرجى كتابة الايميل بشكل صحيح " , Toast.LENGTH_LONG).show();
                                 else{
                                 String message=task.getException().getMessage();
-                                Toast.makeText(forgetp.this," حدث خطأ ما ! "+message,Toast.LENGTH_SHORT).show();}
+                                Toast.makeText(forgetp.this," حدث خطأ ما ! "+message,Toast.LENGTH_LONG).show();}
 
                             }
                         }
