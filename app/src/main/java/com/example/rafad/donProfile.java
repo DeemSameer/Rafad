6c328ee0b75dacc862c95ab88766937e482d71b3
nadafjj <70699190+nadafjj@users.noreply.github.com>
2020-09-28 00:52:07 +0300

    profile pic and ui

--- app/src/main/java/com/example/rafad/donProfile.java
+++ app/src/main/java/com/example/rafad/donProfile.java
@@ -1,5 +1,6 @@
 package com.example.rafad;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
@@ -11,21 +12,28 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class donProfile extends AppCompatActivity {
 
     private static final int GALLERY_INTENT_CODE=1023;
     TextView fullname , email , phone , pass;
-    Button changeProfileIMG;
+    Button changeProfileIMG, resetPassLocal, resendCode;
     FirebaseAuth fAuth;
     FirebaseFirestore fstore;
     String userID;
     FirebaseUser user;
     ImageView profileImage;
+    StorageReference storageRefrence;
 
 
     @Override
@@ -37,6 +45,10 @@ public class donProfile extends AppCompatActivity {
         email = findViewById(R.id.userEmail);
         profileImage= findViewById(R.id.profileImg);
         changeProfileIMG=findViewById(R.id.change);
+
+        fAuth = FirebaseAuth.getInstance();
+        fstore = FirebaseFirestore.getInstance();
+        storageRefrence = FirebaseStorage.getInstance().getReference();
 
 
         changeProfileIMG.setOnClickListener(new View.OnClickListener() {
@@ -59,10 +71,29 @@ public class donProfile extends AppCompatActivity {
             if (resultCode == Activity.RESULT_OK){
                 Uri imageUri = data.getData();
                 profileImage.setImageURI(imageUri);
+                //upload to firebase
+                uploadImageToFirebase(imageUri);
+
             }
         }
    }
 
+    private void uploadImageToFirebase(Uri imageUri) {
+        //to upload image to firebase
+        StorageReference fileRef = storageRefrence.child("profile.jpg");
+        fileRef.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(donProfile.this, " تم تحميل الصورة بنجاح ", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(donProfile.this, " فشل تحميل الصورة ", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    }
 
 
-}
