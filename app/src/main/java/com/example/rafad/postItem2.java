215a44589276e8bb641a27a3084f0bec56f4deb8
Atheer <55921315+ah918@users.noreply.github.com>
2020-09-30 03:54:26 +0300

    POST ITEM #2

--- app/src/main/java/com/example/rafad/postItem2.java
+++ app/src/main/java/com/example/rafad/postItem2.java
@@ -8,27 +8,42 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
 
+import java.lang.ref.Reference;
+import java.util.HashMap;
+import java.util.Map;
+
 public class postItem2 extends AppCompatActivity {
 
-
+    public static final String TAG = "TAG";
     FirebaseAuth fAuth;
-    Button changePostImage;
+    Button changePostImage,share;
     ImageView postImage;
     StorageReference storageReference;
-
+    EditText descerption;
+    Uri imageUri;
+    String itemID;
+    String idImage;
+    Reference postRef;
+    FirebaseFirestore fStore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,8 +54,11 @@ public class postItem2 extends AppCompatActivity {
         postImage=findViewById(R.id.postImage);
         changePostImage=findViewById(R.id.chosepicture);
         storageReference = FirebaseStorage.getInstance().getReference();
+        descerption=findViewById(R.id.descrption);
+        fStore=FirebaseFirestore.getInstance();
 
 
+        share = findViewById(R.id.button3);
 
         changePostImage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,6 +76,36 @@ public class postItem2 extends AppCompatActivity {
             }
         });
 
+        share.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+
+                uploadImageToFirebase(imageUri);
+
+                String des = descerption.getText().toString();
+                String cat ="book";
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                itemID = database.getReference("item").push().getKey();
+                DocumentReference documentrefReference = fStore.collection("item").document(itemID);
+                //postRef=idImage;
+                //store data
+                Map<String, Object> post = new HashMap<>();
+                post.put("image", idImage);
+                post.put("description", des);
+                post.put("cat", cat);
+
+                //check the add if it's success or not
+                documentrefReference.set(post).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "posted successfully " + itemID);
+                    }
+                });
+
+                startActivity(new Intent(postItem2.this, homepageDonator.class));
+                finish();
+            }
+        });
     }
 
     @Override
@@ -66,10 +114,11 @@ public class postItem2 extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 1000){
             if (resultCode == Activity.RESULT_OK){
-                Uri imageUri = data.getData();
-                //profileImage.setImageURI(imageUri);
+                imageUri = data.getData();
+
+                postImage.setImageURI(imageUri);
                 //upload to firebase
-                uploadImageToFirebase(imageUri);
+                //uploadImageToFirebase(imageUri);
 
             }
         }
@@ -77,7 +126,10 @@ public class postItem2 extends AppCompatActivity {
 
     private void uploadImageToFirebase(Uri imageUri) {
         //to upload image to firebase
-        final StorageReference fileRef = storageReference.child("items/"+fAuth.getCurrentUser().getUid()+"post.jpg");
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        String newID = database.getReference("item").push().getKey();
+        idImage="items/"+newID+"post.jpg";
+        final StorageReference fileRef = storageReference.child(idImage+"post.jpg");
         fileRef.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
             @Override
             public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
