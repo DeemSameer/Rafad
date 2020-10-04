d61b0e3a785425aa1debb0d0ae40994a320b795c
a-alsultan <arob2604@gmail.com>
2020-10-05 00:21:45 +0300

    list view for main profile

--- app/src/main/java/com/example/rafad/postinfo.java
+++ app/src/main/java/com/example/rafad/postinfo.java
@@ -3,11 +3,13 @@ package com.example.rafad;
 import android.net.Uri;
 
 public class postinfo {
-    Uri imageUri;
+    String itemID;
+    String imageID;
     String des;
     String cat;
-    public postinfo(Uri imageUri1 , String des1 , String cat1){
-        imageUri = imageUri1;
+    public postinfo(String itemID1, String imageUri1 , String des1 , String cat1){
+        itemID = itemID1;
+        imageID = imageUri1;
         des = des1;
         cat=cat1;
 
