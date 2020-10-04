491bac01811b1c2749bb306d8fe30aa8b257c7d1
nadafjj <70699190+nadafjj@users.noreply.github.com>
2020-10-05 01:20:28 +0300

    ben profile 90% finished

--- app/src/main/java/com/example/rafad/benEditProfile.java
+++ app/src/main/java/com/example/rafad/benEditProfile.java
@@ -7,14 +7,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.DocumentsContract;
 import android.provider.MediaStore;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -31,48 +28,34 @@ import com.squareup.picasso.Picasso;
 import java.util.HashMap;
 import java.util.Map;
 
-public class donProfile extends AppCompatActivity {
-
-    private static final int GALLERY_INTENT_CODE=1023;
-    public static final String TAG = "TAG";
-    Button changeProfileIMG, cancell, saveBtn, changePassBtn;
+public class benEditProfile extends AppCompatActivity {
+    EditText profileFullNameBEn, profileEmailBEn, profilePhoneNumberBEn;
+    Button saveChanges, changeProfileIMG, cancelBtn1;
+    FirebaseUser user;
     FirebaseAuth fAuth;
     FirebaseFirestore fstore;
-    String userID;
-    FirebaseUser user;
-    ImageView profileImage;
     StorageReference storageRefrence;
-    EditText profileFullName , profileEmail, profilePhoneNumber;
+    ImageView profileImage;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_don_profile);
+        setContentView(R.layout.ben_edit_profile);
 
-        profileImage= findViewById(R.id.profileImg);
-        changeProfileIMG=findViewById(R.id.change);
-        cancell=findViewById(R.id.cancel);
-        saveBtn = findViewById(R.id.save);
-        changePassBtn = findViewById(R.id.changepass);
+        profileFullNameBEn =findViewById(R.id.benNewName);
+        profileEmailBEn =findViewById(R.id.benNewEmail);
+        profilePhoneNumberBEn =findViewById(R.id.benNewPhone);
+        saveChanges =findViewById(R.id.saveBenChange);
+        profileImage =findViewById(R.id.profileImgBen2);
+        changeProfileIMG = findViewById(R.id.changeBenImg);
+        cancelBtn1 =findViewById(R.id.c1);
 
 
-        profileFullName =findViewById(R.id.newName);
-        profileEmail = findViewById(R.id.newEmail);
-        profilePhoneNumber = findViewById(R.id.newPhone);
-
+        user = fAuth.getCurrentUser();
         fAuth = FirebaseAuth.getInstance();
         fstore = FirebaseFirestore.getInstance();
         storageRefrence = FirebaseStorage.getInstance().getReference();
-        user = fAuth.getCurrentUser();
-
-        changePassBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(), changePasswordDon.class));
-                finish();
-            }
-        });
 
 
         //pring data from mainProfile activity
@@ -81,74 +64,83 @@ public class donProfile extends AppCompatActivity {
         final String email = data.getStringExtra("email");
         final String phone = data.getStringExtra("phone");
 
-        profileEmail.setText(email);
-        profileFullName.setText(fullName);
-        profilePhoneNumber.setText(phone);
-
-        //to check if passed or not
-        Log.d(TAG, "onCreate: " + fullName + " " + email + " " + phone);
+        profileEmailBEn.setText(email);
+        profileFullNameBEn.setText(fullName);
+        profilePhoneNumberBEn.setText(phone);
 
 
-        saveBtn.setOnClickListener(new View.OnClickListener() {
+        cancelBtn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(benEditProfile.this, BenMainProfile.class));
+                finish();
+
+            }
+        });
+
+
+
+
+        saveChanges.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //extract what the user entr
-                if (profileFullName.getText().toString().isEmpty()){
-                    Toast.makeText(donProfile.this, " لا يمكن ترك الإسم فارغًا  ", Toast.LENGTH_LONG).show();
+                if (profileFullNameBEn.getText().toString().isEmpty()){
+                    Toast.makeText(benEditProfile.this, " لا يمكن ترك الإسم فارغًا  ", Toast.LENGTH_LONG).show();
                     return;
                 }
-                if (profileEmail.getText().toString().isEmpty()){
-                    Toast.makeText(donProfile.this, " لا يمكن ترك البريد الإلكتروني فارغًا ", Toast.LENGTH_LONG).show();
+                if (profileEmailBEn.getText().toString().isEmpty()){
+                    Toast.makeText(benEditProfile.this, " لا يمكن ترك البريد الإلكتروني فارغًا ", Toast.LENGTH_LONG).show();
                     return;
                 }
-                if (profilePhoneNumber.getText().toString().isEmpty()){
-                    Toast.makeText(donProfile.this, " لا يمكن ترك رقم الجوال فارغًا ", Toast.LENGTH_LONG).show();
+                if (profilePhoneNumberBEn.getText().toString().isEmpty()){
+                    Toast.makeText(benEditProfile.this, " لا يمكن ترك رقم الجوال فارغًا ", Toast.LENGTH_LONG).show();
                     return;
 
                 }
-                if (profilePhoneNumber.getText().toString().length()!=10){
-                    profilePhoneNumber.setError(" يجب أن يتكون رقم الجوال من 10 أرقام ");
+                if (profilePhoneNumberBEn.getText().toString().length()!=10){
+                    profilePhoneNumberBEn.setError(" يجب أن يتكون رقم الجوال من 10 أرقام ");
                     return;
                 }
 
-                if (!profilePhoneNumber.getText().toString().substring(0,2).equals("05")){
-                    profilePhoneNumber.setError(" يجب أن يبدأ رقم الجوال بـ 05 ");
+                if (!profilePhoneNumberBEn.getText().toString().substring(0,2).equals("05")){
+                    profilePhoneNumberBEn.setError(" يجب أن يبدأ رقم الجوال بـ 05 ");
                     return;
                 }
 
 
 
                 //extract the email to change it
-                final String email2 = profileEmail.getText().toString();
+                final String email2 = profileEmailBEn.getText().toString();
                 user.updateEmail(email2).addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
-                        DocumentReference docRef = fstore.collection("donators").document(user.getUid());
-                       //put new data to firebase
+                        DocumentReference docRef = fstore.collection("beneficiaries").document(user.getUid());
+                        //put new data to firebase
                         Map<String,Object> edited = new HashMap<>();
                         edited.put("email", email2);
-                        edited.put("userName", profileFullName.getText().toString());
-                        edited.put("phoneNumber", profilePhoneNumber.getText().toString());
+                        edited.put("userName", profileFullNameBEn.getText().toString());
+                        edited.put("phoneNumber", profilePhoneNumberBEn.getText().toString());
                         docRef.update(edited).addOnSuccessListener(new OnSuccessListener<Void>() {
                             @Override
                             public void onSuccess(Void aVoid) {
 
-                                if(profileFullName.getText().toString().equals(fullName) && profileEmail.getText().toString().equals(email) && profilePhoneNumber.getText().toString().equals(phone)){
-                                    startActivity(new Intent(getApplicationContext(), mainProfile.class));
+                                if(profileFullNameBEn.getText().toString().equals(fullName) && profileEmailBEn.getText().toString().equals(email) && profilePhoneNumberBEn.getText().toString().equals(phone)){
+                                    startActivity(new Intent(benEditProfile.this, BenMainProfile.class));
                                     finish();
                                 }
 
                                 else {
-                                    Toast.makeText(donProfile.this, " تم تحديث الملف الشخصي بنجاح ", Toast.LENGTH_LONG).show();
-                                startActivity(new Intent(getApplicationContext(), mainProfile.class));
-                                finish();}
+                                    Toast.makeText(benEditProfile.this, " تم تحديث الملف الشخصي بنجاح ", Toast.LENGTH_LONG).show();
+                                    startActivity(new Intent(benEditProfile.this, BenMainProfile.class));
+                                    finish();}
                             }
                         });
 
                         if (!email.equals(email2)){
                             fAuth.getCurrentUser().sendEmailVerification();
 
-                            Toast.makeText(donProfile.this, " تم تحديث البريد الإلكتروني بنجاح، يرجى اتباع الرابط المرسل لتفعيله ", Toast.LENGTH_LONG).show();
+                            Toast.makeText(benEditProfile.this, " تم تحديث البريد الإلكتروني بنجاح، يرجى اتباع الرابط المرسل لتفعيله ", Toast.LENGTH_LONG).show();
                         }
 
                     }
@@ -157,14 +149,14 @@ public class donProfile extends AppCompatActivity {
                     public void onFailure(@NonNull Exception e) {
                         String exMsg = e.getMessage();
                         if (exMsg.equals("The email address is already in use by another account.")){
-                            Toast.makeText(donProfile.this, "البريد الإلكتروني موجود مسبقًا  ", Toast.LENGTH_LONG).show();
+                            Toast.makeText(benEditProfile.this, "البريد الإلكتروني موجود مسبقًا  ", Toast.LENGTH_LONG).show();
                         }
 
                         if (exMsg.equals("This operation is sensitive and requires recent authentication. Log in again before retrying this request.")){
-                            Toast.makeText(getApplicationContext(),  "هذه العملية حساسة وتتطلب مصادقة حديثة. قم بتسجيل الدخول مرة أخرى قبل إعادة محاولة هذا الطلب.", Toast.LENGTH_LONG).show();
+                            Toast.makeText(benEditProfile.this,  "هذه العملية حساسة وتتطلب مصادقة حديثة. قم بتسجيل الدخول مرة أخرى قبل إعادة محاولة هذا الطلب.", Toast.LENGTH_LONG).show();
                         }
                         else {
-                            Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_LONG).show();
+                            Toast.makeText(benEditProfile.this,e.getMessage(),Toast.LENGTH_LONG).show();
                         }
 
                     }
@@ -194,21 +186,12 @@ public class donProfile extends AppCompatActivity {
             }
         });
 
-
-
-
-        cancell.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(donProfile.this, mainProfile.class));
-                finish();
-
-            }
-        });
-
     }
-   @Override
-   protected void onActivityResult(int requestCode, int resultCode, @androidx.annotation.Nullable Intent data){
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @androidx.annotation.Nullable Intent data){
 
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 1000){
@@ -220,25 +203,25 @@ public class donProfile extends AppCompatActivity {
 
             }
         }
-   }
+    }
     private void uploadImageToFirebase(Uri imageUri) {
         //to upload image to firebase
         final StorageReference fileRef = storageRefrence.child("users/"+fAuth.getCurrentUser().getUid()+"profile.jpg");
         fileRef.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
             @Override
             public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-              fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                  @Override
-                  public void onSuccess(Uri uri) {
-                      Picasso.get().load(uri).into(profileImage);
-                  }
-              });
+                fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        Picasso.get().load(uri).into(profileImage);
+                    }
+                });
 
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
-                Toast.makeText(donProfile.this, " فشل تغيير الصورة ", Toast.LENGTH_SHORT).show();
+                Toast.makeText(benEditProfile.this, " فشل تغيير الصورة ", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -247,6 +230,15 @@ public class donProfile extends AppCompatActivity {
 
     }
 
-    }
+}
+
+
+
+
+
+
+
+
+
 
 
