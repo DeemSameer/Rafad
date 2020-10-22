fc672f3edfadb678b45f4c9ab2e61a675bf76a84
Atheer <55921315+ah918@users.noreply.github.com>
2020-10-22 14:06:40 +0300

    delete item

--- app/src/main/java/com/example/rafad/homePage.java
+++ app/src/main/java/com/example/rafad/homePage.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,6 +15,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
@@ -21,12 +23,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class homePage extends AppCompatActivity {
-    Button logout, profile;
+    Button logout, profile, clothes , furniture,device,all;
     FirebaseAuth fAuth;
     FirebaseFirestore fStore;
 
     //////// for view list of items
     List<postinfo> arrayItem=new ArrayList<>();
+    List<postinfo> arrayItemC=new ArrayList<>();
+    List<postinfo> arrayItemD=new ArrayList<>();
+    List<postinfo> arrayItemF=new ArrayList<>();
+    List<postinfo> arrayItemA=new ArrayList<>();
     public static final String TAG = "TAG";
     ListView listView;
     //////// above is for view list of items
@@ -64,6 +70,119 @@ public class homePage extends AppCompatActivity {
     });
 
 
+        clothes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clothes.setPaintFlags(clothes.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+                Query p =  fStore.collection("item").whereEqualTo("Catogery", "ملابس")
+                        .whereEqualTo("isRequested", "no" );
+
+                p.get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    for (QueryDocumentSnapshot document : task.getResult()) {
+                                        Log.d(TAG, document.getId() + " => " + document.getData());
+                                        arrayItemC.add(new postinfo((String) document.getId(), (String) document.get("User id"), (String) document.get("Image"), (String) document.get("Description"), (String) document.get("Catogery"), (String) document.get("Title"),(String) document.get("isRequested") ));
+                                        Log.d(TAG, "SIZE item list => " + arrayItemC.size());
+                                    }
+                                    ListViewAdaptorBen adapter = new ListViewAdaptorBen(homePage.this, arrayItemC);
+                                    listView = (ListView) findViewById(R.id.postedlistHomePage);
+                                    listView.setAdapter(adapter);
+                                } else {
+                                    Log.d(TAG, "Error getting documents: ", task.getException());
+                                }
+                            }
+                        });
+
+            }
+        });
+        furniture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Query p =  fStore.collection("item").whereEqualTo("Catogery", "أثاث")
+                        .whereEqualTo("isRequested", "no" );
+
+                p.get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    for (QueryDocumentSnapshot document : task.getResult()) {
+                                        Log.d(TAG, document.getId() + " => " + document.getData());
+                                        arrayItemF.add(new postinfo((String) document.getId(), (String) document.get("User id"), (String) document.get("Image"), (String) document.get("Description"), (String) document.get("Catogery"), (String) document.get("Title"),(String) document.get("isRequested") ));
+                                        Log.d(TAG, "SIZE item list => " + arrayItemF.size());
+                                    }
+                                    ListViewAdaptorBen adapter = new ListViewAdaptorBen(homePage.this, arrayItemF);
+                                    listView = (ListView) findViewById(R.id.postedlistHomePage);
+                                    listView.setAdapter(adapter);
+                                } else {
+                                    Log.d(TAG, "Error getting documents: ", task.getException());
+                                }
+                            }
+                        });
+
+            }
+        });
+        device.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Query p =  fStore.collection("item").whereEqualTo("Catogery", "أجهزة")
+                        .whereEqualTo("isRequested", "no" );
+
+                p.get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    for (QueryDocumentSnapshot document : task.getResult()) {
+                                        Log.d(TAG, document.getId() + " => " + document.getData());
+                                        arrayItemD.add(new postinfo((String) document.getId(), (String) document.get("User id"), (String) document.get("Image"), (String) document.get("Description"), (String) document.get("Catogery"), (String) document.get("Title"),(String) document.get("isRequested") ));
+                                        Log.d(TAG, "SIZE item list => " + arrayItemD.size());
+                                    }
+                                    ListViewAdaptorBen adapter = new ListViewAdaptorBen(homePage.this, arrayItemD);
+                                    listView = (ListView) findViewById(R.id.postedlistHomePage);
+                                    listView.setAdapter(adapter);
+                                } else {
+                                    Log.d(TAG, "Error getting documents: ", task.getException());
+                                }
+                            }
+                        });
+
+            }
+        });
+
+        all.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Query p =  fStore.collection("item")
+                        .whereEqualTo("isRequested", "no" );
+
+                p.get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    for (QueryDocumentSnapshot document : task.getResult()) {
+                                        Log.d(TAG, document.getId() + " => " + document.getData());
+                                        arrayItemA.add(new postinfo((String) document.getId(), (String) document.get("User id"), (String) document.get("Image"), (String) document.get("Description"), (String) document.get("Catogery"), (String) document.get("Title"),(String) document.get("isRequested") ));
+                                        Log.d(TAG, "SIZE item list => " + arrayItemA.size());
+                                    }
+                                    ListViewAdaptorBen adapter = new ListViewAdaptorBen(homePage.this, arrayItemA);
+                                    listView = (ListView) findViewById(R.id.postedlistHomePage);
+                                    listView.setAdapter(adapter);
+                                } else {
+                                    Log.d(TAG, "Error getting documents: ", task.getException());
+                                }
+                            }
+                        });
+
+            }
+        });
         //////////////////// for list of items second try////////////////////////
 
 
