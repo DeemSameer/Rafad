ae987fa7917f1835c44d3aba892c347cd1de003a
a-alsultan <arob2604@gmail.com>
2020-11-02 10:01:45 +0300

    chat2

---
+++ app/src/main/java/com/example/rafad/BassAppLicChat.java
@@ -0,0 +1,14 @@
+package com.example.rafad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BassAppLicChat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bass_app_lic_chat);
+    }
+}
