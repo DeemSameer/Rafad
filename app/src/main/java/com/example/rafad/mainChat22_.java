8735a5d9d0b692c3132030bfd48744bd342d6165
nadafjj <70699190+nadafjj@users.noreply.github.com>
2020-11-04 05:37:24 +0300

    no errors and no result ;(

--- app/src/main/java/com/example/rafad/mainChat22_.java
+++ app/src/main/java/com/example/rafad/mainChat22_.java
@@ -1,13 +1,22 @@
 package com.example.rafad;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TableLayout;
 import android.widget.TextView;
 import android.widget.Toolbar;
 
+import com.bumptech.glide.Glide;
+import com.example.rafad.Model.user;
+import com.google.common.collect.Table;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -15,6 +24,8 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -32,6 +43,12 @@ public class mainChat22_ extends AppCompatActivity {
         setContentView(R.layout.main_chat2);
 
         Toolbar toolbar= findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+        //setSupportActionBar().setTitle("");
+
+
+
+
         username=findViewById(R.id.username);
         profile_image=findViewById(R.id.profile_image);
 
@@ -41,6 +58,14 @@ public class mainChat22_ extends AppCompatActivity {
         reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                user user1=dataSnapshot.getValue(user.class);
+                username.setText(user1.getUsername());
+                if(user1.getImageURL().equals("default")){
+                    profile_image.setImageResource(R.mipmap.ic_launcher);
+                }else {
+                    Glide.with(mainChat22_.this).load(user1.getImageURL()).into(profile_image);
+
+                }
 
             }
 
@@ -48,6 +73,53 @@ public class mainChat22_ extends AppCompatActivity {
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
-        })
+        });
+
+        TableLayout tableLayout=findViewById(R.id.tab_layout);
+        ViewPager viewPager=findViewById(R.id.view_pager);
+
+        ViewPagerAdapter viewPagerAdapter=new ViewPagerAdapter(getSupportFragmentManager());
+
+    }
+
+    private void setSupportActionBar(Toolbar toolbar) {
+    }
+
+
+
+    class ViewPagerAdapter extends FragmentPagerAdapter{
+
+        private ArrayList<Fragment> fragments;
+        private ArrayList<String> titles;
+
+        ViewPagerAdapter(FragmentManager fm){
+            super(fm);
+            this.fragments=new ArrayList<>();
+            this.titles=new ArrayList<>();
+
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+
+        public void ddFragment(Fragment fragment, String title ){
+            fragments.add(fragment);
+            titles.add(title);
+        }
+
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position){
+            return titles.get(position);
+        }
     }
 }
