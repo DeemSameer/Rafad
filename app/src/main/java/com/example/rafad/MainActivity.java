9df31cd661024b7e0c527f7e7f016ddd53e9fe50
Deem Sameer AlDekhial <66889295+DeemSameer@users.noreply.github.com>
2020-11-26 13:33:15 +0300

    ...

--- app/src/main/java/com/example/rafad/MainActivity.java
+++ app/src/main/java/com/example/rafad/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.rafad;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -25,12 +28,20 @@ public class MainActivity extends AppCompatActivity {
     public static final String TAG = "TAG";
     Timer timer ;
 
-
+    ImageView x,y;
+    Animation frombottom,fromtop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        x=(ImageView)findViewById(R.id.top);
+        y=(ImageView)findViewById(R.id.bottom);
+        frombottom= AnimationUtils.loadAnimation(this,R.anim.frombottom);
+        y.setAnimation(frombottom);
+
+        fromtop= AnimationUtils.loadAnimation(this,R.anim.fromtop);
+        x.setAnimation(fromtop);
         timer = new Timer();
         timer.schedule(new TimerTask() {
             @Override
